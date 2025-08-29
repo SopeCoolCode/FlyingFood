19239ea1ca07a410dec4277ae2f18da12c972e73
SopeCoolCode <sope@coolcode.io>
2025-08-29 16:51:52 +0200

    Initial commit

---
+++ Assets/zz_Packages/FastFoodRestaurant/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace SojaExiles
+{
+	public class OpenCloseDoor : MonoBehaviour
+	{
+		[SerializeField] public Transform player;
+		private Animator animator;
+		private bool open;
+
+		private void OnMouseOver()
+		{
+			if (player)
+			{
+				float dist = Vector3.Distance(player.position, transform.position);
+				if (dist < 15)
+				{
+					if (Input.GetMouseButtonDown(0))
+					{
+						StartCoroutine(open ? Close() : Open());
+					}
+				}
+			}
+		}
+
+		private IEnumerator Open()
+		{
+			print("you are opening the door");
+			animator.Play("Opening");
+			open = true;
+			yield return new WaitForSeconds(.5f);
+		}
+
+		private IEnumerator Close()
+		{
+			print("you are closing the door");
+			animator.Play("Closing");
+			open = false;
+			yield return new WaitForSeconds(.5f);
+		}
+
+        private void Awake()
+        {
+			animator = GetComponent<Animator>();
+        }
+    }
+}
---
+++ Assets/zz_Packages/Helpers/ShowCursor.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Awake : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
